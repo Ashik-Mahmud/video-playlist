c2f014a73acfa4f95991aafb97aa9f740e9f5fc9
ashikmahmud <ashikmamud187@gmail.com>
2022-02-01 00:45:36 +0600

    Added next previous button system in video application not perfectly

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@
 // 1. Set Default video in video player.
 // 2. First get a video lsits in browser from array 
 // 3. Create a function for consictency play video 
-
+// 4. Create next prev button functions
 
 
 // selections 
@@ -10,6 +10,8 @@ const videoPlayer = document.querySelector(".video-player video"),
     videoTitle = document.querySelector(".video-player .video-title"),
     lists = document.querySelector(".lists"),
     videoCount = document.querySelector(".videoCount"),
+    nextBtn = document.querySelector(".btns-group .next-btn"),
+    prevBtn = document.querySelector(".btns-group .prev-btn"),
     videoLists = document.querySelectorAll(".single-video");
 
 
@@ -58,3 +60,39 @@ function playVideo(videolist) {
     videoTitle.textContent = videoName;
 
 };
+
+
+// work with next prev button options 
+var index = 0;
+prevBtn.classList.add("disabled");
+function nextPlay() {
+    if (index > videoArray.length - 1) {
+        nextBtn.classList.add("disabled");
+        index = 0;
+    } else {
+        console.log(videoArray[index]);
+        videoPlayer.src = `./assets/media/${videoArray[index].url}`;
+        videoTitle.textContent = videoArray[index].name;
+        videoPlayer.play();
+        prevBtn.classList.remove("disabled");
+        index++
+    }
+};
+function prevPlay() {
+
+    if (index < 0) {
+        prevBtn.classList.add("disabled");
+        index = 0;
+    } else {
+        console.log(videoArray[index]);
+        videoPlayer.src = `./assets/media/${videoArray[index].url}`;
+        videoTitle.textContent = videoArray[index].name;
+        videoPlayer.play();
+        nextBtn.classList.remove("disabled");
+        index--
+    }
+
+
+};
+nextBtn.addEventListener("click", nextPlay)
+prevBtn.addEventListener("click", prevPlay)
